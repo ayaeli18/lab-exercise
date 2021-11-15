8d063364b4aa68b5774fc2b9f8b5b2fea4c721de
Ryunosuke Senda <42127342+Ryu-Sen@users.noreply.github.com>
2021-11-15 22:52:02 +0900

    added original

---
+++ original.cpp
@@ -0,0 +1,193 @@
+int main() 
+
+{ 
+
+//add code here 
+
+ 
+
+do{ 
+
+//add code here 
+
+  sagot=getMenu(choice); 
+
+    }while(); 
+
+     
+
+    { 
+
+    	case '1'://bubblesort 
+
+    	  {      
+
+    	   	        //add code here 
+
+    	  	           //call getInput function 
+
+    	  	       
+
+//call getBubble function 
+
+//call convert2DimSort 
+
+                     
+
+                   
+
+  } 
+
+    		break; 
+
+    	case '2': 
+
+    		{ 
+
+    			 
+
+    			//add code here 
+
+    	  	           //call getInput function 
+
+    	  	       
+
+//call getSelect function 
+
+//call convert2DimSort 
+
+} 
+
+    		break; 
+
+    	case '3'://heapsort 
+
+    		         { 
+
+    		         	//add code here 
+
+    		         	getHeap(arr,size); 
+
+    		         	cout<<endl<<"Heapyfied values ..."<<endl; 
+
+//add code here 
+
+ } 
+
+    		break; 
+
+    	case '4': cout<<"end of program.....\n"; 
+
+    		      exit(1); 
+
+} 
+
+} 
+
+ 
+
+///////////////////////////////// 
+
+char getMenu(char ans) 
+
+{ 
+
+//add code here 
+
+ 
+
+} 
+
+//////////////////////////////////// 
+
+void getInput(int list[],int dim)// for both option 1 and 2 only 
+
+{ 
+
+//add code here 
+
+} 
+
+////////////////////////////////////// 
+
+void convert2DimSort(int list[],int dim) 
+
+{ 
+
+ 
+
+  //add codehere 
+
+   
+
+   get2DOut(inList);  
+
+} 
+
+//////////////////////////////////////////////// 
+
+void get2DOut(int list1[][sizeCol]) 
+
+{ 
+
+ 
+
+  cout<<"Ascending Order\n"; 
+
+//add code here 
+
+ 
+
+ cout<<"\nDescending Order\n"; 
+
+//add code here 
+
+//////////////////////////////////////////// 
+
+void getBubble(int list[],int dim) 
+
+{ 
+
+//solution to bubble sort 
+
+    	  	//add code here 
+
+} 
+
+/////////////////////////////////////////////// 
+
+void getSelect(int list[],int dim) 
+
+{ 
+
+//solution to selection sort   
+
+    	  	//add code here 
+
+ 
+
+} 
+
+////////////////////////////////////////////// 
+
+void getHeap(int list3[],int dimen) 
+
+{ 
+
+//add code here 
+
+  
+
+} 
+
+/////////////////////////////////////////////// 
+
+void tryAgain() 
+
+{ 
+
+  //add code here 
+
+ 
+
+} 
